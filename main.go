64e2efe0637991847e4b2ff803690b9901b20969
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-08-27 16:14:35 +0700

    Golang Rest API

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gorilla/mux"
+
+func main() {
+	// Init Router
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books", deleteBook).Methods("DELETE")
+}
